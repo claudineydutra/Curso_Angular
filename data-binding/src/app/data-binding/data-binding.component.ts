410afc6f6758335da9b65feba0fd7b03de9e167d
Claudiney <claudineygui@gmail.com>
2022-11-05 22:28:18 -0300

    first commit

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  // styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight{
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]
+})
+export class DataBindingComponent implements OnInit {
+  url: string = "http://claudiney.com";
+  cursoAngular: boolean = true;
+  urlImagem: string = "https://i.pinimg.com/236x/04/43/40/0443407d1834ee924063e0dc8362a842.jpg";
+
+  valorAtual = '';
+  valorSalvo = '';
+  MouseOverOut: boolean = false;
+
+  pessoa = {
+    nome: "default",
+    idade: 18   
+  }
+
+
+  getValor(): number{
+    return 1;
+  }
+
+  getCurtirCurso(): boolean{
+    return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.MouseOverOut = !this.MouseOverOut;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
